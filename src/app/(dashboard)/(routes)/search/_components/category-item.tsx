a4127c706290d86b98ed02d648eeb104672b4c88
Lalu Ibnu Hidayatullah <laluibnuhidayatullah@gmail.com>
2023-10-15 08:48:07 +0700

    Categories Query

---
+++ src/app/(dashboard)/(routes)/search/_components/category-item.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import React from "react";
+import { IconType } from "react-icons";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
+
+import { cn } from "@/lib/utils";
+
+interface CategoryItemProps {
+  label: string;
+  value?: string;
+  icon?: IconType;
+}
+const CategoryItem: React.FC<CategoryItemProps> = ({
+  label,
+  value,
+  icon: Icon,
+}) => {
+  const pathName = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const currentCategoryId = searchParams.get("categoryId");
+  const currentTitle = searchParams.get("title");
+
+  const isSelected = currentCategoryId === value;
+
+  const onClick = () => {
+    const url = qs.stringifyUrl(
+      {
+        url: pathName,
+        query: {
+          title: currentTitle,
+          categoryId: isSelected ? null : value, // ini jika sudah terpilih maka akan di unselect
+        },
+      },
+      { skipEmptyString: true, skipNull: true }
+    );
+
+    router.push(url);
+  };
+
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "border-slate-200 py-2 px-3 rounded-full flex items-center gap-x-1 hover:border-sky-700 transition hover:text-sky-700",
+        isSelected && "bg-sky-200/20 text-sky-800 border-sky-700"
+      )}
+      type="button"
+    >
+      {Icon && <Icon size={20} />}
+      <div className="truncate">{label}</div>
+    </button>
+  );
+};
+
+export default CategoryItem;
