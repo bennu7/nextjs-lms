aab55afde92ebce13c60ed313c3133e899776f05
Lalu Ibnu Hidayatullah <laluibnuhidayatullah@gmail.com>
2023-10-19 22:43:02 +0700

    Stripe Integration

--- src/app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ src/app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { auth } from "@clerk/nextjs";
+import { File } from "lucide-react";
 
 import { getChapter } from "@/actions/get-chapter";
 import { Banner } from "@/components/banner";
+import { Separator } from "@/components/ui/separator";
+import { Preview } from "@/components/preview";
 
 import { VideoPlayer } from "./_components/video-player";
+import { CourseEnrollButton } from "./_components/course-enroll-button";
 
 const ChapterIdPage = async ({
   params,
@@ -63,6 +67,41 @@ const ChapterIdPage = async ({
             completeOnEnd={completeOnEnd}
           />
         </div>
+        <div className="p-4 flex flex-col md:flex-row items-center justify-between">
+          <h2 className="text-2xl font-semibold mb-2">{chapter.title}</h2>
+          {purchase ? (
+            // TODO: add CourseProgressButton
+            <div></div>
+          ) : (
+            <CourseEnrollButton
+              courseId={params.courseId}
+              price={course.price!}
+            />
+          )}
+        </div>
+        <Separator />
+        <div>
+          <Preview value={chapter.desc!} />
+        </div>
+        {!!attachments?.length && (
+          <>
+            <Separator />
+            <div className="p-4">
+              {attachments.map((attachment) => (
+                <a
+                  href={attachment.url}
+                  key={attachment.id}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="flex items-center p-3 w-full bg-sky-200 text-sky-700 border rounded-md hover:underline"
+                >
+                  <File />
+                  <p className="line-clamp-1">{attachment.name}</p>
+                </a>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
